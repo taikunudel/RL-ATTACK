f2fed42577e857832db651d1e1f684dd2a3688a2
taikun <taikun@udel.edu>
2025-05-06 12:38:31 -0400

    Add attack-genai subfolder

---
+++ attack-genai/run_all_sh.sh
@@ -0,0 +1,8 @@
+# /usa/taikun/07_transencoder/attack-genai/run_all_attacks.sh
+#!/usr/bin/env bash
+
+bash "/usa/taikun/07_transencoder/attack-genai/run_eva_attack_genai copy 2.sh"
+bash "/usa/taikun/07_transencoder/attack-genai/run_eva_attack_genai copy 3.sh"
+bash "/usa/taikun/07_transencoder/attack-genai/run_eva_attack_genai copy 4.sh"
+bash "/usa/taikun/07_transencoder/attack-genai/run_eva_attack_genai copy 5.sh"
+bash "/usa/taikun/07_transencoder/attack-genai/run_eva_attack_genai copy 6.sh"
